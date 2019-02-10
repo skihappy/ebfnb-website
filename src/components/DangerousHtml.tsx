4fcf35f6f70963b0ce5486876fbaeda877db854b
skihappy <skihappy@hotmail.com>
2019-02-09 20:24:32 -0800

    Added DangerousHtml component. (#13)
    * Added DangerousHtml component.
    
    * Removed console.log().
    
    * Refactored component.

---
+++ src/components/DangerousHtml.tsx
@@ -0,0 +1,48 @@
+import { navigate } from '@reach/router'
+import React, { useRef, useEffect } from 'react'
+
+const clickListener = event => {
+  const {
+    defaultPrevented,
+    button,
+    metaKey,
+    altKey,
+    ctrlKey,
+    shiftKey,
+    target: { href },
+  } = event
+  const shouldNavigate =
+    !defaultPrevented &&
+    button === 0 &&
+    !(metaKey || altKey || ctrlKey || shiftKey)
+  if (shouldNavigate) {
+    event.preventDefault()
+    navigate(href)
+  }
+}
+
+const hostname = window ? window.location.hostname : ''
+
+const processInternalLinks = (
+  operation: 'add' | 'remove',
+  ref: React.MutableRefObject<any>
+) =>
+  ref.current.querySelectorAll('a').forEach(anchor => {
+    if (hostname === anchor.hostname || !anchor.hostname.length) {
+      anchor[operation]('click', clickListener)
+    }
+  })
+
+const DangerousHtml = ({ html, ...props }: { html: string }) => {
+  const ref = useRef()
+
+  useEffect(() => {
+    processInternalLinks('addEventListener', ref)
+    return () => processInternalLinks('removeEventListener', ref)
+  }, [ref.current])
+
+  /* eslint-disable-next-line react/no-danger */
+  return <div ref={ref} dangerouslySetInnerHTML={{ __html: html }} {...props} />
+}
+
+export default DangerousHtml
