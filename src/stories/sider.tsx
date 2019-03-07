60be2fc3ed13539ada7aedcf5851ac811a42175e
jspoet <skihappy17@gmail.com>
2019-03-06 22:27:16 -0800

    Added animatedSider section to sider story.

--- src/stories/sider.tsx
+++ src/stories/sider.tsx
@@ -6,38 +6,40 @@ import Sider from '../components/Sider'
 
 export default jsx
 
+const OPACITY_DURATION = '.8s'
+const SCALE_DURATION = '.25s'
+
 const MenuItem = (props: any) => (
   <li>
     <button type="button" {...props} />
   </li>
 )
 
-const AnimatedSider = ({ isOpen = false, ...restProps }: any) => {
-  console.log('animated', isOpen)
-  const animationCss = css`
-    transform-origin: left;
-    transform: scale(0, 1});
-    transition: opacity 2500ms, transform 2500ms 2500ms;
-    //opacity: 0;
-    width: fit-content;
-    &[isOpen='true'] {
-      transform: scale(1, 1);
-      opacity: 1;
-      transition: transform 2500ms, opacity 2500ms 2500ms;
-    }
-  `
-  return (
-    <Sider
-      css={animationCss}
-      isOpen={isOpen}
-      {...{ isOpen, ...restProps, side: 'left' }}
-    />
-  )
-}
+const AnimatedSider = ({ isOpen = false, ...restProps }: any) => (
+  <Sider
+    css={css`
+      transform-origin: left;
+      transform: scale(0, 1);
+      transition: opacity ${OPACITY_DURATION},
+        transform ${SCALE_DURATION} ${OPACITY_DURATION};
+      opacity: 0;
+      width: fit-content;
+      &[data-is-open='true'] {
+        transform: scale(1, 1);
+        opacity: 1;
+        transition: transform ${SCALE_DURATION},
+          opacity ${OPACITY_DURATION} ${SCALE_DURATION};
+      }
+    `}
+    data-is-open={isOpen}
+    {...{ isOpen, ...restProps, side: 'left' }}
+  />
+)
 
 const App = ({ AppSider = Sider }: any) => {
   const [isSiderOpen, setIsSiderOpen] = useState(false)
   const toggleSider = () => setIsSiderOpen(!isSiderOpen)
+
   return (
     <div>
       {/* Header */}
