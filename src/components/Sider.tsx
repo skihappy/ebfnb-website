b623e18eb40c39fbc1614ee279305a5ce9f94e2e
jspoet <skihappy17@gmail.com>
2019-03-06 17:20:40 -0800

    Added sider story.

--- src/components/Sider.tsx
+++ src/components/Sider.tsx
@@ -20,68 +20,6 @@ interface SiderProps {
   children?: any
 }
 
-// const useSider = ({
-//   side = Side.left,
-//   size = 'fit-content',
-//   isOpen = true,
-//   ...attrs
-// }: SiderProps) => {
-//   const prevValues = useMemo({ isOpen })
-//   const
-//   const [fullDimention, controlledDimention] =
-//     side === 'top' || side === 'bottom'
-//       ? ['width', 'height']
-//       : ['height', 'width']
-//   const axis = side === 'top' || side === 'bottom' ? 'Y' : 'X'
-//   const siderCss = [
-//     css`
-//       position:absolute;
-//       ${fullDimention}: 100%;
-//       max-${controlledDimention}: 100%;
-//       ${controlledDimention}:${size};
-//     `,
-//     !isOpen &&
-//       css`
-//         ${controlledDimention}: 0px;
-//       `,
-//     // siderEffect({ side, action }),
-//   ]
-
-//   const opacityTo = opacity => keyframes`
-//     to {opacity:${opacity}}
-//   `
-//   const scaleTo = scale => keyframes`
-//     to {transform: scale${axis}(${scale})}
-//   `
-//   const slideOpen = keyframes`
-//     to {transform: scale${axis}(1)}
-//   `
-//   const slideClosed = keyframes`
-//     to {transform: scale${axis}(0)}
-//   `
-//   const animateOpen = `${scaleTo(1)} ${SLIDE_DURATION},${opacityTo(
-//     0
-//   )} ${CHANGE_OPACITY_DURATION}`
-
-//   const animateClose = `${opacityTo(1)} ${CHANGE_OPACITY_DURATION},${scaleTo(
-//     0
-//   )} ${SLIDE_DURATION}`
-
-//   const openCss = css`
-//     animation: ${slideOpen} 2s, ${opacityTo(1)} 2s 2s;
-//   `
-//   const closedCss = css`
-//     animation: ${opacityTo(0)} 2s, ${slideClosed} 2s 2s;
-//   `
-//   return [
-//     css`
-//       transform-origin: ${side};
-//       animation-fill-mode: both;
-//     `,
-//     action === 'open' ? openCss : closedCss,
-//   ]
-// }
-
 const Sider = ({
   children = null,
   side = Side.left,
